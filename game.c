e4a3d45c2ca29bb9b55569bc58ac1883694669c7
Zalfapanian <aymericpapegnies@gmail.com>
2025-04-01 17:43:00 +0200

    Update game.c

--- game.c
+++ game.c
@@ -24,3 +24,33 @@ int jouer(SDL_Renderer* renderer) {
 
     return continuer;
 }
+
+    while(continuer){ //coeur du jeu ici, les actions seront repété pour faire le déplacement des différentes images, ...
+        
+        struct Personnage
+        {
+            SDL_Texture **image; //une liste des images (de pointeur sur image pour être précis) quicontiendra l’ensemble des sprites de notre personnages (ici mario).
+
+            SDL_Rect position; //la position en l’instant t de notre personnage mario et plus particulièrement du carré qui le symbolise.
+            
+            int jump; //symbolise si le personnage saute ou non, vaudra 1 à partir du moment ou le personnage commence à sauter et vaudra 0 à partir du moment où le personnage redescend (ou si le personnage ne saute pas).
+            
+            int jumptime; //correspond à la durée depuis que le personnage effectue un saut.
+            
+            int gravite; //vaut 1 si le personnage est dans les airs (et donc que la gravité doit s’appliquer), vaut 0 sinon. Ce champ est utilisé pour tomber si il n’y a pas de sol en dessous du personnage.
+            
+            int direction; 
+            
+            //ou
+            
+            int derniereDirection; //symbolise la direction (ou la direction précédente) vers laquelle mario se dirige, vaut 1 pour la droite, 2 pour la gauche et 0 pour une absence de mouvement.
+            
+            int temps; //doit être incrémenté de manière régulière et sera utilisé pour faire varier les sprites de mario pour donner une impression de mouvement.
+            
+            int win; //symbolise plus l’état de la partie, vaut -1 si le personnage perds (tombe dans le vide par exemple), 1 si il gagne en touchant le drapeau et 0 sinon.
+
+        };
+        
+        
+        
+    }
