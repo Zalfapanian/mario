487104d9f70417ed66a61e8cc18413e3234d6372
Zalfapanian <aymericpapegnies@gmail.com>
2025-04-01 17:41:17 +0200

    Update charactere.c

--- charactere.c
+++ charactere.c
@@ -6,3 +6,17 @@
 void freePersonnage(Personnage* mario, Personnage **goomba, int nbGoomba) {
     
 }
+
+void createperso(){
+    struct Personnage mario;
+    mario.jump = 0;
+    mario.jumptime = 0;
+    mario.gravite = 0;
+    mario.direction = 0;
+    mario.dernierdirection = 0;
+    mario.temp = 0;
+    mario.win = 0;
+    mario.invisible = 0;
+    mario.lvl = 0;
+    mario.lvlperdu = 0;
+}
